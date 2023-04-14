a6566a99550f2e8dea5794b5587fb0df3227bc5b
Motkov01500 <liuboslavmotkov@gmail.com>
2023-04-14 15:25:13 +0300

    initialeMapStructProject

---
+++ testMapStruct/src/main/java/com/mapstruct/mapstructtest/MapstructtestApplication.java
@@ -0,0 +1,13 @@
+package com.mapstruct.mapstructtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructtestApplication.class, args);
+	}
+
+}
---
+++ testMapStruct/src/test/java/com/mapstruct/mapstructtest/MapstructtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapstruct.mapstructtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
